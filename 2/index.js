16a3ba1b69702911f5e0f3e3864b6ed63fb50a92
Sardor <sardoribodov07@gmail.com>
2023-12-04 22:53:45 +0500

    2. Basic data-types

---
+++ 2/index.js
@@ -0,0 +1,11 @@
+// 1. Syntax of declaring variables
+//     let / const variableName: data-type = value***
+// 2. Basic data-types in TS:
+//     1. number
+//     2. string
+//     3. boolean
+//     4. null
+//     5. undefined
+//     6. object (why {} not just object)
+//     let / const variableName: { key: value … } = { … }***
+//     7. any (avoid using this type as much as possible)
